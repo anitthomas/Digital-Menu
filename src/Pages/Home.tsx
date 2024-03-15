7b8c9c3e59839c31f0d40886e9b8f4a44e35feef
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-15 15:22:59 +0530

    Update Home page layout and add sign-in buttons

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,8 +3,18 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
     return (
         <div >
-            <div className="signup h-screen w-[40%] bg-orange-500 ">
-                
+            <div className="signup flex flex-col justify-center items-center h-screen w-[40%] bg-orange-500 ">
+
+                <div className="header ml-[10vh]">
+                    <h2 className=" scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-white">
+                        Book your Orders Digitally
+                    </h2>
+                </div>
+                <div className="signup-button w-[80%] justify-evenly mt-[10vh] flex ">
+                    <Button variant="outline">Sign-in as User</Button>
+                    <Button variant="outline">Sign-In as Restaurant</Button>
+
+                </div>
 
             </div>
         </div>
