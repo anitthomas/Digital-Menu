9e207b3ccb318bba3b604afa15dd2628e5aff966
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-15 14:57:21 +0530

    Refactor Home component to include a signup section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
     return (
-        <div>
-            <Button>Click me</Button>
+        <div >
+            <div className="signup h-screen w-[40%] bg-orange-500 ">
+                
+
+            </div>
         </div>
     )
 }
